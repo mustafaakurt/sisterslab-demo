87fcd83e194b673bf1b85f1544eca1bd8668ce9f
Mustafa Kurt <mustafakurt2000@gmail.com>
2024-03-01 17:20:44 +0300

    init commit

---
+++ src/main/java/com/mustafaakurt/sisterslabdemo/SisterslabdemoApplication.java
@@ -0,0 +1,13 @@
+package com.mustafaakurt.sisterslabdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisterslabdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisterslabdemoApplication.class, args);
+	}
+
+}
